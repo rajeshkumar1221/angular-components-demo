f25e5342b8fa9c843d3e957d6e2bd2dbe91c60cd
rajeshkumar1221 <rajeshkumar1221@gmail.com>
2025-09-09 19:27:45 +0530

    Completed the user and admin components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { User } from "./components/user/user";
+import { Admin } from "./components/admin/admin";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, User, Admin],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  standalone: true
 })
 export class App {
   protected readonly title = signal('angular-components-demo');
+  
 }
--- src/app/components/user/user.ts
+++ src/app/components/user/user.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';  
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './user.html',
   styleUrl: './user.css'
 })
-export class User {
+export class User implements OnInit, OnDestroy {
+  user = { name: '', email: '' };
+  ngOnInit(): void {
+    console.log("User component initialized.");
+  }
 
+  ngOnDestroy(): void {
+    console.log("User component destroyed.");
+  }
+  onSubmit() {
+    console.log('User Form Submitted:', this.user);
+  } 
 }
+
